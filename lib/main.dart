dd824cefe382d013574cc2d31fcfabddb3f9fa3e
Apsara Bishwokarma <apsarabishwokarma7@gmail.com>
2023-01-16 13:30:37 +0545

    theme extraction

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_learning/pages/home_page.dart';
 import 'package:flutter_learning/pages/login_page.dart';
 import 'package:flutter_learning/utils/routes.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_learning/widgets/theme.dart';
 
 void main() {
   runApp(const MyApp()); //runApp is a method
@@ -19,12 +19,15 @@ class MyApp extends StatelessWidget {
 //home: HomePage(),
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          primaryTextTheme: GoogleFonts.latoTextTheme()),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+
+      //ThemeData(
+      //     primarySwatch: Colors.deepPurple,
+      //     primaryTextTheme: GoogleFonts.latoTextTheme()),
+      // darkTheme: ThemeData(
+      //   brightness: Brightness.dark,
+      // ),
       initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": (context) => const LoginPage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,6 +9,11 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        //sabai main dart with theme widget
+        //   backgroundColor: Colors.white,
+        //    elevation: 0.0,
+        //  iconTheme: const IconThemeData(color: Colors.black),
+        //   textTheme: Theme.of(context).textTheme),
         //horizontal bar
         title: const Text("Catalog App"),
       ),
